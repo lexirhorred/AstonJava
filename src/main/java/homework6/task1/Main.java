a19fbde44e30f71d25aae704ee4cd407f74f55c5
egor <e.zemlyanskov@gmail.com>
2024-06-03 08:43:49 +0300

    02.06.24

--- src/main/java/homework6/task1/Main.java
+++ src/main/java/homework6/task1/Main.java
@@ -2,10 +2,14 @@ package homework6.task1;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println(Login.authorization("HelloHelloHelloHelloHello", "o1pb3", "o1pb3"));
-        System.out.println(Login.authorization("Hello HelloHello", "hh5hh", "hh5hh"));
-        System.out.println(Login.authorization("HelloHelloHello", "hh5hh", "hh5hh"));
-        System.out.println(Login.authorization("HelloHelloHello", "hh5hh6", "hh5hh5"));
-        System.out.println(Login.authorization("HelloHelloHello", "hh5 hh5", "hh5 hh5"));
+        try {
+            System.out.println(Login.authorization("HelloHelloHelloHelloHello", "o1pb3", "o1pb3"));
+            System.out.println(Login.authorization("Hello HelloHello", "hh5hh", "hh5hh"));
+            System.out.println(Login.authorization("HelloHelloHello", "hh5hh", "hh5hh"));
+            System.out.println(Login.authorization("HelloHelloHello", "hh5hh6", "hh5hh5"));
+            System.out.println(Login.authorization("HelloHelloHello", "hh5 hh5", "hh5 hh5"));
+        } catch (WrongLoginException | WrongPasswordException e) {
+            System.out.println("Authorization error: " + e.getMessage());
+        }
     }
 }
